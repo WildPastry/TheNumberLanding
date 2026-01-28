417ecdb7e370a0d290ac3214b552a8a761359ab5
WildPastry <mparkernzdev@gmail.com>
2026-01-29 07:56:02 +1300

    Bump versions. Add website link.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,16 @@ const Footer: React.FC = (): JSX.Element => {
   const currentDate: number = new Date().getFullYear();
   return (
     <section className='absolute right-3 bottom-3'>
-      <p className='font-outfit-light text-l'>© {currentDate} Mike Parker</p>
+      <div className='flex gap-1'>
+        <p className='font-outfit-light text-l'>© {currentDate}</p>
+        <a
+          className='font-outfit-light text-l text-flame hover:text-watermelon'
+          href='https://mikeparker.co.nz/'
+          rel='noopener noreferrer'
+          target='_blank'>
+          Mike Parker
+        </a>
+      </div>
     </section>
   );
 };
