81007088bd6a9fc30ba8c930fad125d1f1d04e5f
WildPastry <mparkernzdev@gmail.com>
2026-01-31 22:19:26 +1300

    Adjust privacy policy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,8 @@ const Home: React.FC = (): JSX.Element => {
         <h1 className='text-(--fg) font-cubano text-5xl pt-1'>THE</h1>
         <h1 className='text-water font-cubano text-100'>NUMBER</h1>
       </div>
-      <div className='py-2 px-3 bg-(--banner) mb-10'>
-        <h2 className='text-(--bg) font-bebas text-5xl pt-1'>
-          A date tracking app
-        </h2>
+      <div className='py-2 px-3 bg-(--main-banner) mb-10'>
+        <h2 className='text-(--bg) text-5xl pt-1'>A date tracking app</h2>
       </div>
       <Badges />
     </section>
